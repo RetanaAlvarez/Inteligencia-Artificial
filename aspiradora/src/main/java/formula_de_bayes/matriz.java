5ded77c6a949def603a5e779ef84c71192433633
RetanaAlvarez <retanaalvarez@gmail.com>
2022-07-29 15:29:54 -0500

    modificando codigo

---
+++ aspiradora/src/main/java/formula_de_bayes/matriz.java
@@ -0,0 +1,32 @@
+
+package formula_de_bayes;
+
+
+public class matriz {
+
+    public static String campos(String campo[]) {
+        String[] a = {"#_inst", "Estado", "Tempe", "Humedad", "viento", "Clase"};
+        return null;
+    }
+
+     public static String registros(String registros[][]) {
+        String[][] a = {
+        {"1", "soleado", "85", "85", "No", "No jugar"},
+        {"2", "soleado", "80", "90", "Si", "No jugar"},
+        {"3", "Nublado", "83", "78", "No", "Jugar"},
+        {"4", "lluvio", "70", "96", "No", "Jugar"},
+        {"5", "lluvio", "68", "80", "No", "Jugar"},
+        {"6", "lluvio", "65", "70", "Si", "No jugar"},
+        {"7", "Nublado", "64", "65", "Si", "Jugar"},
+        {"8", "soleado", "72", "95", "No", "No jugar"},
+        {"9", "soleado", "69", "70", "No", "Jugar"},
+        {"10", "lluvio", "75", "80", "No", "Jugar"},
+        {"11", "soleado", "75", "70", "Si", "Jugar"},
+        {"12", "Nublado", "72", "90", "Si", "jugar"},
+        {"13", "Nublado", "81", "75", "No", "Jugar"},
+        {"14", "lluvio", "71", "80", "Si", "No jugar"}};
+        return null;
+    }
+    
+
+}
